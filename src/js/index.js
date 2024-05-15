860c7978ce617695755c6f144cbb4a1f3c35c36b
Lucas Nunes <lucas.jlnunes@gmail.com>
2024-05-15 11:28:06 -0300

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+const menu = document.querySelector('#menu');
+const ul = document.querySelector('ul')
+const div = document.querySelector('.listMobile')
+const nav = document.querySelector('header')
+const menuInit = document.querySelector('.menuInit')
+
+
+menu.addEventListener('click', e => {
+
+
+
+
+  if(menu.classList.contains('menuInit')){
+    menu.classList.remove('menuInit')
+    div.style.display = 'none'
+  } else {
+    div.style.display = 'block'
+    console.log('oi')
+    menu.classList.add('menuInit')
+  }
+})
